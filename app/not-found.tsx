4465041845ac788896ace13605b974c3cc76b7f5
clarencepenz <clarencedespenz@gmail.com>
2024-02-13 22:03:17 +0100

    chore: change color

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ export default function Custom404() {
   return (
     <div className="container-screen h-screen flex flex-col gap-8 justify-center items-center">
       <h4 className="text-black font-bold md:text-4xl">404 - You are lost</h4>
-      <Link href="/" className="transition-colors hover:text-evfy-primary/80 hover:underline"> Go Back </Link>
+      <Link href="/" className="text-evfy-primary transition-colors hover:text-evfy-primary/60 hover:underline"> Go Back </Link>
     </div>
   );
 }
