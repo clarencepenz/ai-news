18762216ea77de4e374b2626720a1a9cd506f825
clarencepenz <clarencedespenz@gmail.com>
2024-05-24 17:38:05 +0100

    fix: add metadata back

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,17 @@
-"use client";
 import React from "react";
 
+import type { Metadata } from "next";
 import { Rubik } from "next/font/google";
 import "./globals.css";
 import cn from "@/lib/utils";
 import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
+import { DEFAULT_METADATA } from "@/constants/seo";
 
 const rubikTitle = Rubik({ subsets: ["latin"], variable: "--title-font" });
 const rubikBody = Rubik({ subsets: ["latin"], variable: "--body-font" });
+
+export const metadata: Metadata = DEFAULT_METADATA;
 
 export default function RootLayout({
   children,
